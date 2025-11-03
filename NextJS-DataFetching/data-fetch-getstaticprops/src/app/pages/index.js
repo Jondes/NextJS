1a0fe5f6d8d60a34b70bee0b70bae1dec43d54be
Johnson Guedes <jondes560@hotmail.com>
2025-11-03 14:42:40 +0000

    Create index.js

---
+++ NextJS-DataFetching/data-fetch-getstaticprops/src/app/pages/index.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const Home = ({ posts }) => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        {posts.map((post) => (  
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+                {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+  return {
+    props: { posts },
+  };
+} 
+export default Home;
